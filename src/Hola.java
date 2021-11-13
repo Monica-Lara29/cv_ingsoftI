d881cfa2c32eb41b789ed7980c70b952133e3f20
hidroinfo <hidroinfo@hidroinfo-HP-ProBook-4440s>
2021-11-13 15:39:50 -0500

    Rama mezclada

--- src/Hola.java
+++ src/Hola.java
@@ -4,6 +4,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("version Mónica");
+     System.out.println("Rama mezclada");
 	}
 
 }
--- src/saludo.java
+++ src/saludo.java
@@ -4,6 +4,7 @@ public class saludo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hola Mónica");
+      
 	}
 
 }
